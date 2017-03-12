7c13f21f8e0192f036b155bac2228d3585ee0ca4
John FitzGerald <iconium9000@gmail.com>
2017-03-12 14:51:11 -0700

    Added Node

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+var serv = require('http').Server(app)
+var io = require('socket.io')(serv,{})
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/client/index.html')
+})
+app.use('/client',express.static(__dirname + '/client'))
+
+serv.listen(2000)
+console.log("Server Active")
+
+
+io.sockets.on('connection', function(socket) {
+	
+	console.log('socket connection')
+	
+	
+})
+
