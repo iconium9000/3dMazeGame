13bbd7bd4cf02eef4081618b68483365e282796b
John FitzGerald <iconium9000@gmail.com>
2017-03-13 15:34:35 -0700

    Formating Changes

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ console.log('app.js: init')
 var express = require('express')
 var app = express()
 var serv = require('http').Server(app)
-var io = require('socket.io')(serv,{})
+var io = require('socket.io')(serv, {})
 var fs = require('fs')
 
 var pt = require('./client/point.js')
 var mg = require('./client/game.js')
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/client/index.html')
+    res.sendFile(__dirname + '/client/index.html')
 })
-app.use('/client',express.static(__dirname + '/client'))
+app.use('/client', express.static(__dirname + '/client'))
 
 serv.listen(2000)
 
@@ -36,20 +36,20 @@ console.log("Server Active")
 var sockets = []
 
 io.sockets.on('connection', function(socket) {
-	do {
-		socket.id =
-		 Math.random()
-	} while (sockets[socket.id] != null)
-	sockets[socket.id] = socket
-	console.log('socket connection ' + socket.id)
-	socket.emit('handShake')
-	socket.on('disconnect',function() {
-		console.log('disconnect socket ' + socket.id)
-		delete sockets[socket.id]
-	})
-	socket.on('msg',function(msg){
-		console.log(msg)
-	})
+    do {
+        socket.id =
+            Math.random()
+    } while (sockets[socket.id] != null)
+    sockets[socket.id] = socket
+    console.log('socket connection ' + socket.id)
+    socket.emit('handShake')
+    socket.on('disconnect', function() {
+        console.log('disconnect socket ' + socket.id)
+        delete sockets[socket.id]
+    })
+    socket.on('msg', function(msg) {
+        console.log(msg)
+    })
 })
 
 // ----------------------------------------
--- client/game.js
+++ client/game.js
@@ -1,105 +1,118 @@
 console.log("game.js: init")
 try {
-  pt = require('./point.js')
-  console.log('game.js: init point.js')
-} catch(e){
-  if (pt != null) {
-    console.log('game.js: rcv point.js')
-  } else {
-    throw 'game.js: point.js not found'
-  }
+    pt = require('./point.js')
+    console.log('game.js: init point.js')
+} catch (e) {
+    if (pt != null) {
+        console.log('game.js: rcv point.js')
+    } else {
+        throw 'game.js: point.js not found'
+    }
 }
-function getString(p){
-  return `${p.x},${p.y}`
+
+function getString(p) {
+    return `${p.x},${p.y}`
 }
 var mg = {
-  shift: pt.zero(),
-  cellSize: 20,
-  cellWidth: 15,
-  modes: {
-    'shift':{
-      order: 0,
-      draw: function(g,i){
-        g.strokeStyle = 'white'
-        var y = mg.cellSize * (1 + 2 * i)
-        pt.drawLine(g,{
-      		a: {x: 5 + mg.cellSize - mg.cellWidth,y:5 + y},
-      		b: {x: 5 + mg.cellSize + mg.cellWidth,y:5 + y}
-      	})
-      	pt.drawLine(g,{
-      		a: {x: 5 + mg.cellSize,y:5 + y - mg.cellWidth},
-      		b: {x: 5 + mg.cellSize,y:5 + y + mg.cellWidth}
-      	})
-      }
-    },
-    'newCell':{
-      order:1,
-      draw: function(g,i){
-        var y = mg.cellSize * (1 + 2 * i)
-        
-      }
+    shift: pt.zero(),
+    cellSize: 20,
+    cellWidth: 15,
+    modes: {
+        'shift': {
+            order: 0,
+            draw: function(g, i) {
+                g.strokeStyle = 'white'
+                var y = mg.cellSize * (1 + 2 * i)
+                pt.drawLine(g, {
+                    a: {
+                        x: 5 + mg.cellSize - mg.cellWidth / 2,
+                        y: 5 + y
+                    },
+                    b: {
+                        x: 5 + mg.cellSize + mg.cellWidth / 2,
+                        y: 5 + y
+                    }
+                })
+                pt.drawLine(g, {
+                    a: {
+                        x: 5 + mg.cellSize,
+                        y: 5 + y - mg.cellWidth / 2
+                    },
+                    b: {
+                        x: 5 + mg.cellSize,
+                        y: 5 + y + mg.cellWidth / 2
+                    }
+                })
+            }
+        },
+        'newCell': {
+            order: 1,
+            draw: function(g, i) {
+                var y = mg.cellSize * (1 + 2 * i)
+
+            }
+        }
     }
-  }
 }
 mg.mode = mg.modes['shift']
 
 var Level = mg.Level = class {
-  constructor(){
-    this.cells = []
-  }
-  addCell(p){
-    p = mg.Cell.get(p)
-    var s = getString(p)
-    var c = this.cells[s]
-    if (this.cells[s] == null) {
-      c = this.cells[s] = new mg.Cell(p,this)
+    constructor() {
+        this.cells = []
     }
-  }
-  draw(g){
-    for(var i in this.cells){
-      this.cells[i].draw(g)
+    addCell(p) {
+        p = mg.Cell.get(p)
+        var s = getString(p)
+        var c = this.cells[s]
+        if (this.cells[s] == null) {
+            c = this.cells[s] = new mg.Cell(p, this)
+        }
     }
-  }
+    draw(g) {
+        for (var i in this.cells) {
+            this.cells[i].draw(g)
+        }
+    }
 }
 mg.Cell = class {
-  constructor(p,l) {
-    this.x = p.x
-    this.y = p.y
-    this.z = 0
-    this.level = l
-    this.isWall = false
-    this.isDoor = false
-    this.isWire = false
-    this.isPad = false
-    this.isPortal = false
-    console.log('newCell: ' + getString(this))
-  }
-  draw(g){
-    var p = pt.sum(pt.scale(this,mg.cellSize),mg.shift)
-    g.strokeStyle = 'white'
-    g.rect(p.x,p.y,mg.cellSize,mg.cellSize)
-    g.stroke()
-  }
+    constructor(p, l) {
+        this.x = p.x
+        this.y = p.y
+        this.z = 0
+        this.level = l
+        this.isWall = false
+        this.isDoor = false
+        this.isWire = false
+        this.isPad = false
+        this.isPortal = false
+        console.log('newCell: ' + getString(this))
+    }
+    draw(g) {
+        var p = pt.sum(pt.scale(this, mg.cellSize), mg.shift)
+        g.strokeStyle = 'white'
+        g.rect(p.x, p.y, mg.cellWidth, mg.cellWidth)
+        g.stroke()
+    }
 }
 mg.Cell.get = function(p) {
-  return pt.floor(pt.factor(pt.sub(p,mg.shift),mg.cellSize))
+    return pt.floor(pt.factor(pt.sub(p, mg.shift), mg.cellSize))
 }
 mg.Player = class {
-  constructor() {
+    constructor() {
 
-  }
+    }
 }
 mg.Player = class {
-  constructor() {
+    constructor() {
 
-  }
+    }
 }
 mg.Gate = class {
-  constructor() {
+    constructor() {
 
-  }
+    }
 }
 
 try {
-  module.exports = mg
+    module.exports = mg
 } catch (e) {}
