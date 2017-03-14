145b0a37bd1ec9472ee28238b5b046aa6d893c4c
John FitzGerald <iconium9000@gmail.com>
2017-03-14 10:42:09 -0700

    Formating changes

--- client/game.js
+++ client/game.js
@@ -1,106 +1,151 @@
 console.log("game.js: init")
 try {
-    pt = require('./point.js')
-    console.log('game.js: init point.js')
-} catch (e) {
-    if (pt != null) {
-        console.log('game.js: rcv point.js')
-    } else {
-        throw 'game.js: point.js not found'
-    }
+	pt = require('./point.js')
+	console.log('game.js: init point.js')
+}
+catch (e) {
+	if (pt != null) {
+		console.log('game.js: rcv point.js')
+	}
+	else {
+		throw 'game.js: point.js not found'
+	}
 }
 
 function getString(p) {
-    return `${p.x},${p.y}`
+	return {
+		s: `${p.x},${p.y}`,
+		x: p.x,
+		y: p.y,
+		z: p.z
+	}
 }
 var mg = {
-    shift: pt.zero(),
-    cellSize: 20,
-    cellWidth: 15,
-    mode: 'pan',
-    modes: {
-        'pan': {
-            draw: function(g, i) {
-                g.fillStyle = 'white'
-                g.fillText('Pan', 5 + mg.cellSize, 5 + mg.cellSize * (1 + 2 * i))
-            }
-        },
-        'removeCell': {
-            draw: function(g, i) {
-                g.fillStyle = 'white'
-                g.fillText('Del', 5 + mg.cellSize, 5 + mg.cellSize * (1 + 2 * i))
-            }
-        },
-        'newCell': {
-            draw: function(g, i) {
-                g.fillStyle = 'white'
-                g.fillText('NwC', 5 + mg.cellSize, 5 + mg.cellSize * (1 + 2 * i))
-            }
-        }
-    }
+	shift: pt.zero(),
+	cellSize: 20,
+	cellWidth: 15,
+	colors: {
+		space: 'rgba(64, 64, 64, 0.3)',
+		wall: 'rgb(128, 128, 128)',
+	},
+	mode: 'pan',
+	modes: {
+		'pan': {
+			draw: function (g, i) {
+				g.fillStyle = 'white'
+				var cs = mg.cellSize
+				g.fillText('Pan', 5 + cs, 5 + cs * (1 + 2 * i))
+			}
+		},
+		'removeCell': {
+			draw: function (g, i) {
+				g.fillStyle = 'white'
+				var cs = mg.cellSize
+				g.fillText('Del', 5 + cs, 5 + cs * (1 + 2 * i))
+			}
+		},
+		'addCell': {
+			draw: function (g, i) {
+				g.fillStyle = 'white'
+				var cs = mg.cellSize
+				g.fillText('NwC', 5 + cs, 5 + cs * (1 + 2 * i))
+			}
+		}
+	}
 }
 mg.modeKeys = Object.keys(mg.modes)
 var Level = mg.Level = class {
-    constructor() {
-        this.cells = []
-    }
-    addCell(p) {
-        p = mg.Cell.get(p)
-        var s = getString(p)
-        var c = this.cells[s]
-        if (this.cells[s] == null) {
-            c = this.cells[s] = new mg.Cell(p, this)
-        }
-    }
-    removeCell(p) {
-        p = mg.Cell.get(p)
-        var s = getString(p)
-        var c = this.cells[s]
-        if (c) {
-            console.log('remove ' + s)
-            delete this.cells[s]
-        }
-    }
-    draw(g) {
-        for (var i in this.cells) {
-            this.cells[i].draw(g)
-        }
-    }
+	constructor() {
+		this.cells = []
+	}
+	action(cmd, input, parameter) {
+		function getS() {
+			if (parameter == 'string') {
+				return input
+			}
+			else if (parameter == 'mouse') {
+				return getString(mg.Cell.get(input))
+			}
+			else if (parameter == 'point') {
+				return getString(input)
+			}
+			else {
+				console.log(`invalid ${cmd} parameter: ${parameter}`)
+			}
+		}
+		switch (cmd) {
+			case 'pan':
+				if (input.isDown) {
+					pt.sume(mg.shift, pt.sub(input, input.prev))
+				}
+				break
+			case 'removeCell':
+				if (!input.isDown && !input.ups) {
+					break
+				}
+				var s = getS().s
+				if (this.cells[s]) {
+					this.cells[s].kill()
+					delete this.cells[s]
+				}
+				break
+			case 'addCell':
+				if (!input.isDown && !input.ups) {
+					break
+				}
+				var o = getS()
+				console.log('addCell')
+				if (this.cells[o.s] == null) {
+					this.cells[o.s] = new mg.Cell(o, this)
+				}
+				break
+		}
+	}
+	draw(g) {
+		for (var i in this.cells) {
+			this.cells[i].draw(g)
+		}
+	}
 }
 mg.Cell = class {
-    constructor(p, l) {
-        this.x = p.x
-        this.y = p.y
-        this.z = 0
-        this.level = l
-        this.isWall = false
-        this.isDoor = false
-        this.isWire = false
-        this.isPad = false
-        this.isPortal = false
-        console.log('newCell: ' + getString(this))
-    }
-    draw(g) {
-        var p = pt.sum(pt.scale(this, mg.cellSize), mg.shift)
-        g.strokeStyle = 'grey'
-        p.r = mg.cellWidth
-        g.beginPath()
-        g.rect(p.x, p.y, mg.cellWidth, mg.cellWidth)
-        g.fill()
-    }
+	constructor(p, l) {
+		this.x = p.x
+		this.y = p.y
+		this.z = p.z
+		this.s = p.s
+		this.level = l
+		this.isWall = false
+		this.isDoor = false
+		this.isWire = false
+		this.isPad = false
+		this.isPortal = false
+		console.log('newCell: ' + getString(this).s)
+	}
+	kill() {
+		console.log()
+	}
+	draw(g) {
+		var p = pt.sum(pt.scale(this, mg.cellSize), mg.shift)
+		g.fillStyle = mg.colors.space
+		p.r = mg.cellWidth
+		g.beginPath()
+		g.rect(p.x, p.y, mg.cellWidth, mg.cellWidth)
+		g.fill()
+	}
 }
-mg.Cell.get = function(p) {
-    return pt.floor(pt.factor(pt.sub(p, mg.shift), mg.cellSize))
+mg.Cell.get = function (p) {
+	return pt.floor(pt.factor(pt.sub(p, mg.shift), mg.cellSize))
 }
 mg.Player = class {
-    constructor() {}
+	constructor() {}
 }
 mg.Player = class {
-    constructor() {}
+	constructor() {}
 }
 mg.Gate = class {
-    constructor() {}
+	constructor() {}
 }
 try {
-    module.exports = mg
-} catch (e) {}
+	module.exports = mg
+}
+catch (e) {}
--- client/gameWindow.js
+++ client/gameWindow.js
@@ -1,95 +1,96 @@
 console.log('gameWindow.js: init')
-
 var socket = io()
-
 var canvas = document.getElementById('canvas')
 var g = canvas.getContext('2d')
 var keyDown = []
-var mouseUps = 0
-
+var keyUps = []
 var eventQueue = []
-function update() {
-    mouseUps = 0
-    setTime()
-    getSize()
-    for (var i in eventQueue) {
-        eventQueue[i]()
-    }
-    if (eventQueue.length != 0) {
-      eventQueue = []
-    }
-}
 
+function update() {
+	mouse.ups = 0
+	keyUps = []
+	setTime()
+	getSize()
+	for (var i in eventQueue) {
+		eventQueue[i]()
+	}
+	if (eventQueue.length != 0) {
+		eventQueue = []
+	}
+}
 var now, dt, last
 var w, h, u
-
 var mouse = {
-    x: 0,
-    y: 0,
-    z: 0,
-    isDown: false,
-    hasDragged: false
+	x: 0,
+	y: 0,
+	z: 0,
+	r: 5,
+	isDown: false,
+	hasDragged: false,
+	ups: 0
 }
 
 function setTime() {
-    now = (new Date()).getTime()
-    dt = now - last
-    last = now
+	now = (new Date()).getTime()
+	dt = now - last
+	last = now
 }
 
 function getSize() {
-    w = canvas.width = window.innerWidth - 15
-    h = canvas.height = window.innerHeight - 20
-    u = w > h ? h : w
+	w = canvas.width = window.innerWidth - 15
+	h = canvas.height = window.innerHeight - 20
+	u = w > h ? h : w
 }
-
 // --------------------------------------------
 // ReqFrame
 // --------------------------------------------
-
-var reqFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {window.setTimeout(callback, 30)}
-
+var reqFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+	window.setTimeout(callback, 30)
+}
 // --------------------------------------------
 // Mouse controls
 // --------------------------------------------
 $(canvas).css('cursor', 'none')
+
 function setMouse(e) {
-    mouse.x = e.clientX
-    mouse.y = e.clientY
+	mouse.x = e.clientX
+	mouse.y = e.clientY
 }
-$(document).mousemove(function(e) {
-    eventQueue.push(function() {
-        setMouse(e)
-        mouse.hasDragged = mouse.isDown
-    })
+$(document).mousemove(function (e) {
+	eventQueue.push(function () {
+		setMouse(e)
+		mouse.hasDragged = mouse.isDown
+	})
 })
-$(document).mousedown(function(e) {
-    eventQueue.push(function() {
-        setMouse(e)
-        mouse.hasDragged = false
-        mouse.isDown = true
-    })
+$(document).mousedown(function (e) {
+	eventQueue.push(function () {
+		setMouse(e)
+		mouse.hasDragged = false
+		mouse.isDown = true
+	})
 })
-$(document).mouseup(function(e) {
-    eventQueue.push(function() {
-        setMouse(e)
-        mouse.isDown = false
-        ++mouseUps
-    })
+$(document).mouseup(function (e) {
+	eventQueue.push(function () {
+		setMouse(e)
+		mouse.isDown = false
+		mouse.ups = true
+	})
 })
 // --------------------------------------------
 // Keyboard controls
 // --------------------------------------------
 function etochar(e) {
-    return String.fromCharCode(e.which | 0x20)
+	return String.fromCharCode(e.which | 0x20)
 }
-$(document).keypress(function(e) {
-    eventQueue.push(function() {
-        keyDown[etochar(e)] = true
-    })
+$(document).keypress(function (e) {
+	eventQueue.push(function () {
+		keyDown[etochar(e)] = true
+	})
 })
-$(document).keyup(function(e) {
-    eventQueue.push(function() {
-        keyDown[etochar(e)] = false
-    })
+$(document).keyup(function (e) {
+	eventQueue.push(function () {
+		var c = etochar(e)
+		keyDown[c] = false
+		keyUps[eto] = true
+	})
 })
