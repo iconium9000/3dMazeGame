caa93f07c49c0fa2ed24446f40a0c12846863b30
John FitzGerald <iconium9000@gmail.com>
2017-04-07 14:01:22 -0700

    Minor updates

--- client/game.js
+++ client/game.js
@@ -33,157 +33,41 @@ var mg = {
 	offset: 5,
 	cellSize: 25,
 	cellWidth: 18,
-	wires: {},
+	all: {},
 	cells: {},
-	directions: {
-		'up': {
-			key: 'ArrowUp',
-			x: 0,
-			y: -1,
-			z: 0
-		},
-		'dn': {
-			key: 'ArrowDown',
-			x: 0,
-			y: 1,
-			z: 0
-		},
-		'rt': {
-			key: 'ArrowRight',
-			x: 1,
-			y: 0,
-			z: 0
-		},
-		'lf': {
-			key: 'ArrowLeft',
-			x: -1,
-			y: 0,
-			z: 0
-		},
-	},
-	states: {
-		'space': {
-			color: '#404040',
-			style: 'stroke',
-			shape: 'drawRect',
-			size: 'cellWidth',
-			sizeFactor: 1 / 2
-		},
-		'level': {
-			color: '#800080',
-			style: 'stroke',
-			shape: 'drawCircle',
-			size: 'cellWidth',
-			sizeFactor: 1 / 2
-		},
-		'wire': {
-			color: {
-				true: '#208000',
-				false: '#802000'
-			},
-			style: 'stroke',
-			shape: 'drawRect',
-			size: 'cellWidth',
-			sizeFactor: 1 / 5,
-			foreach: `if (cell.rt) pt.drawLine(g,{a:ptm(cell),b:ptm(cell.rt)})
-			if (cell.lf) pt.drawLine(g,{a:ptm(cell),b:ptm(cell.lf)})`
-		},
-		'door': {
-			color: {
-				true: '#008000',
-				false: '#800000'
-			},
-			style: 'fill',
-			shape: 'fillRect',
-			size: 'cellSize',
-			sizeFactor: 1 / 2
-		},
-		'pad': {
-			color: {
-				true: '#008000',
-				false: '#800000'
-			},
-			style: 'fill',
-			shape: 'square',
-			size: 'cellWidth',
-			sizeFactor: 1 / 3
-		},
-		'portal': {
-			color: {
-				true: '#800080',
-				false: '#800040'
-			},
-			style: 'stroke',
-			shape: 'square',
-			size: 'cellSize',
-			sizeFactor: 1 / 2,
-			foreach: `if (cell.level) pt.drawLine(g,{a:ptm(cell),b:ptm(cell.level)})`
-		},
-		'player': {
-			color: '#FFFFFF',
-			style: 'stroke',
-			shape: 'cross',
-			size: 'cellSize',
-			sizeFactor: 1 / 2,
-			foreach: `if (cell == mg.player) pt.drawRect(g, cell.point)`
-		},
-		'key': {
-			color: '#FFFFFF',
-			style: 'stroke',
-			shape: 'square',
-			size: 'cellWidth',
-			sizeFactor: 1 / 2,
-		},
-		'square': {}
-	},
-	modes: {
-		'game': {
-			key: 'g',
-			str: 'gam',
-		},
-		'pan': {
-			key: 'm',
-			str: 'pan'
-		},
-		'space': {
-			key: 'x',
-			str: 'spc'
-		},
-		'level': {
-			key: 'l',
-			str: 'lvl'
-		},
-		'wire': {
-			key: 'v',
-			str: 'wir'
-		},
-		'door': {
-			key: 'd',
-			str: 'dor'
-		},
-		'pad': {
-			key: 'a',
-			str: 'pad'
-		},
-		'portal': {
-			key: 'p',
-			str: 'ptl'
-		},
-		'player': {
-			key: 'j',
-			str: 'plr'
-		},
-		'key': {
-			key: 'k',
-			str: 'key'
-		},
-		'square': {
-			key: 's',
-			str: 'sqr'
+	init: function(gw) {
+		for (var i in mg.states) {
+			mg.all[i] = {}
+		}
+
+		var ptm = mg.cell.pointToMouse
+		var mtp = mg.cell.mouseToPoint
+		var g = gw.display.g
+
+		for (var i in mg.stateOrder) {
+			var s = mg.states[mg.stateOrder[i]]
+			var drawOrFill = s.style == 'stroke' ? 'draw' : 'fill'
+
+			eval(`s.draw = function(cell){
+				cell.mouse = ptm(cell)
+				cell.mouse.r = mg.${s.size} / ${s.sizeFactor}
+				g.${s.style}Style = ${s.color[true] ? `s.color[cell.gate.open]`: `s.color`}
+				${s.shape == 'cross' ? `var x = pt.zero(), y = pt.zero()
+					x.x = y.y = cell.mouse.r
+					pt.drawLine(g,{a: pt.sum(cell.mouse, x), b: pt.sub(cell.mouse, x)})` :
+					`pt${s.shape == 'square' ?
+				 		`[cell.square ? '${drawOrFill}Rect' : '${drawOrFill}Circle']` :
+						`.${s.shape}`
+					}(g, cell.mouse)`}
+				${s.foreach || ''}
+			}`)
+			console.log(s.draw)
 		}
 	},
-	init: function() {
+	tick: function(gw) {
+		for (var i in mg.stateOrder) {
 
+		}
 	},
 	cell: {
 		mouseToPoint: function(mouse) {
@@ -264,7 +148,7 @@ var mg = {
 				var cell = mg.cell.action.string(status.string)
 
 				for (var i in mg.states) {
-					cell[status.states[i]] = true
+					cell[i] = false
 				}
 				for (var i in status.states) {
 					cell[status.states[i]] = true
@@ -283,8 +167,164 @@ var mg = {
 			}
 		}
 	},
-	tick: function(gw) {
-
+	directions: {
+		'up': {
+			key: 'ArrowUp',
+			x: 0,
+			y: -1,
+			z: 0
+		},
+		'dn': {
+			key: 'ArrowDown',
+			x: 0,
+			y: 1,
+			z: 0
+		},
+		'rt': {
+			key: 'ArrowRight',
+			x: 1,
+			y: 0,
+			z: 0
+		},
+		'lf': {
+			key: 'ArrowLeft',
+			x: -1,
+			y: 0,
+			z: 0
+		},
+	},
+	stateOrder: ['space', 'wall', 'door', 'wire', 'portal', 'level', 'pad', 'key', 'player'],
+	states: {
+		'space': {
+			color: '#404040',
+			style: 'stroke',
+			shape: 'drawRect',
+			size: 'cellWidth',
+			sizeFactor: 2
+		},
+		'level': {
+			color: '#800080',
+			style: 'stroke',
+			shape: 'drawCircle',
+			size: 'cellWidth',
+			sizeFactor: 2
+		},
+		'wall': {
+			color: '#808080',
+			style: 'fill',
+			shape: 'fillRect',
+			size: 'cellSize',
+			sizeFactor: 2
+		},
+		'wire': {
+			color: {
+				true: '#208000',
+				false: '#802000'
+			},
+			style: 'stroke',
+			shape: 'drawRect',
+			size: 'cellWidth',
+			sizeFactor: 6,
+			foreach: `if (cell.rt && cell.rt.wire)
+				pt.drawLine(g,{a:ptm(cell),b:ptm(cell.rt)})
+			if (cell.lf && cell.lf.wire)
+				pt.drawLine(g,{a: cell.mouse, b: ptm(cell.lf)})`
+		},
+		'door': {
+			color: {
+				true: '#008000',
+				false: '#800000'
+			},
+			style: 'fill',
+			shape: 'fillRect',
+			size: 'cellSize',
+			sizeFactor: 2
+		},
+		'pad': {
+			color: {
+				true: '#008000',
+				false: '#800000'
+			},
+			style: 'fill',
+			shape: 'square',
+			size: 'cellWidth',
+			sizeFactor: 3
+		},
+		'portal': {
+			color: {
+				true: '#800080',
+				false: '#800040'
+			},
+			style: 'stroke',
+			shape: 'square',
+			size: 'cellSize',
+			sizeFactor: 2,
+			foreach: `if (cell.level && (mg.mode == 'mode' || mg.mode == 'level'))
+				pt.drawLine(g,{a:cell.mouse,b:ptm(cell.level)})`
+		},
+		'player': {
+			color: '#FFFFFF',
+			style: 'stroke',
+			shape: 'cross',
+			size: 'cellSize',
+			sizeFactor: 2,
+			foreach: `if (cell == mg.player)
+				pt.drawRect(g, cell.mouse)`
+		},
+		'key': {
+			color: '#FFFFFF',
+			style: 'stroke',
+			shape: 'square',
+			size: 'cellWidth',
+			sizeFactor: 2,
+		},
+		'square': {}
+	},
+	modes: {
+		'game': {
+			key: 'g',
+			str: 'gam',
+		},
+		'pan': {
+			key: 'm',
+			str: 'pan'
+		},
+		'space': {
+			key: 'x',
+			str: 'spc'
+		},
+		'level': {
+			key: 'l',
+			str: 'lvl'
+		},
+		'wire': {
+			key: 'v',
+			str: 'wir'
+		},
+		'door': {
+			key: 'd',
+			str: 'dor'
+		},
+		'pad': {
+			key: 'a',
+			str: 'pad'
+		},
+		'portal': {
+			key: 'p',
+			str: 'ptl'
+		},
+		'player': {
+			key: 'j',
+			str: 'plr'
+		},
+		'key': {
+			key: 'k',
+			str: 'key'
+		},
+		'square': {
+			key: 's',
+			str: 'sqr'
+		}
 	}
 }
 
